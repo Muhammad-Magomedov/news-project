07dbb136a42c98e3554f9b6e4d47b535afd0d39d
Mohmad <magomedov951@mail.ru>
2021-05-24 11:59:09 +0300

    Mohmad

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const hbs = require("express-handlebars");
 const app = express()
 app.use(express.static('public'))
 
+const data = require("./data")
+
 app.set("view engine", "handlebars")
 app.engine("handlebars", hbs())
 
@@ -11,7 +13,21 @@ app.engine("handlebars", hbs())
 
 app.get("/", (req, res) => {
   res.render('home', {
-    title: "home"
+    mainNews: data.news.slice(0, 2),
+    sideBar: data.news.slice(2, data.news.length - 1),
+    style: "css/style.css",
+    logo: "css/logotype.svg"
+
+  })
+})
+
+app.get('/sport/:id', (req, res) => {
+  const filtr = data.news.filter(item => item.categoryId === Number(req.params.id))
+  res.render('sport', {
+    main: filtr.slice(0, 2),
+    sideBar: filtr.slice(2, filtr.length),
+    style: "css/style.css",
+    logo: "css/logotype.svg"
   })
 })
 
